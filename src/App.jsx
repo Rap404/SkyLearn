ca5ccc7b84888984ce8c1cc7a13766f9d3f54c08
Rap404 <adibrafli37@gmail.com>
2025-12-16 21:35:51 +0700

    add: materi turunan;

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import Gemini from "./pages/Gemini";
+import BasicLayout from "./layouts/BasicLayout";
+import KelasPage from "./pages/KelasPage";
+import Limit from "./pages/kelas/Limit";
+import Turunan from "./pages/kelas/Turunan";
+import Integral from "./pages/kelas/Integral";
 import TanyaSoalPage from "./pages/TanyaSoalPage";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/tanya-soal" element={<TanyaSoalPage />} />
+        <Route element={<BasicLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/tanya-soal" element={<TanyaSoalPage />} />
+          <Route path="/kelas" element={<KelasPage />} />
+          <Route path="/kelas/Limit" element={<Limit />} />
+          <Route path="/kelas/Turunan" element={<Turunan />} />
+          <Route path="/kelas/Integral" element={<Integral />} />
+        </Route>
+        {/* <Route path="/kelas" element={<BilanganReal />} /> */}
+        <Route path="/gemini" element={<Gemini />} />
       </Routes>
     </>
   );
---
+++ src/Pages/kelas/Integral.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Integral = () => {
+  return (
+    <div className="flex flex-row width-full ">
+      <div className="bg-amber-50">tes</div>
+      <div className="bg-amber-300">tes</div>
+    </div>
+  );
+};
+
+export default Integral;
